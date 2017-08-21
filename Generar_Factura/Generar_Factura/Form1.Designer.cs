622ab54357d4c81f2c247f686b24b3abc77fad1b
GerusNazgul <gerusnazgul@gmail.com>
2017-08-21 13:41:22 -0500

    Subir Archivos

---
+++ Generar_Factura/Generar_Factura/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Generar_Factura
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtRutaArchivos = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnElegirCarpetaArchivos = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEjecutar = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnElegirCarpeta = new System.Windows.Forms.Button();
+            this.txtRutaSelec = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtRutaArchivos
+            // 
+            this.txtRutaArchivos.Location = new System.Drawing.Point(6, 44);
+            this.txtRutaArchivos.Name = "txtRutaArchivos";
+            this.txtRutaArchivos.ReadOnly = true;
+            this.txtRutaArchivos.Size = new System.Drawing.Size(508, 20);
+            this.txtRutaArchivos.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnElegirCarpetaArchivos);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtRutaArchivos);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(520, 111);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos de los Archivos";
+            // 
+            // btnElegirCarpetaArchivos
+            // 
+            this.btnElegirCarpetaArchivos.Location = new System.Drawing.Point(363, 70);
+            this.btnElegirCarpetaArchivos.Name = "btnElegirCarpetaArchivos";
+            this.btnElegirCarpetaArchivos.Size = new System.Drawing.Size(151, 23);
+            this.btnElegirCarpetaArchivos.TabIndex = 0;
+            this.btnElegirCarpetaArchivos.Text = "Elegir carpeta de Archivos";
+            this.btnElegirCarpetaArchivos.Click += new System.EventHandler(this.btnElegirCarpetaArchivos_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ruta de los Archivos:";
+            // 
+            // btnEjecutar
+            // 
+            this.btnEjecutar.Location = new System.Drawing.Point(18, 243);
+            this.btnEjecutar.Name = "btnEjecutar";
+            this.btnEjecutar.Size = new System.Drawing.Size(112, 23);
+            this.btnEjecutar.TabIndex = 2;
+            this.btnEjecutar.Text = "Ejecutar";
+            this.btnEjecutar.UseVisualStyleBackColor = true;
+            this.btnEjecutar.Click += new System.EventHandler(this.btnEjecutar_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnElegirCarpeta);
+            this.groupBox2.Controls.Add(this.txtRutaSelec);
+            this.groupBox2.Location = new System.Drawing.Point(12, 145);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(520, 92);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Donde Guardar el Archivo";
+            // 
+            // btnElegirCarpeta
+            // 
+            this.btnElegirCarpeta.Location = new System.Drawing.Point(402, 56);
+            this.btnElegirCarpeta.Name = "btnElegirCarpeta";
+            this.btnElegirCarpeta.Size = new System.Drawing.Size(112, 23);
+            this.btnElegirCarpeta.TabIndex = 1;
+            this.btnElegirCarpeta.Text = "Guardar Archivo en";
+            this.btnElegirCarpeta.UseVisualStyleBackColor = true;
+            this.btnElegirCarpeta.Click += new System.EventHandler(this.btnElegirCarpeta_Click);
+            // 
+            // txtRutaSelec
+            // 
+            this.txtRutaSelec.Location = new System.Drawing.Point(6, 30);
+            this.txtRutaSelec.Name = "txtRutaSelec";
+            this.txtRutaSelec.ReadOnly = true;
+            this.txtRutaSelec.Size = new System.Drawing.Size(508, 20);
+            this.txtRutaSelec.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(552, 278);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.btnEjecutar);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Generar Facturacion";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtRutaArchivos;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnEjecutar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnElegirCarpeta;
+        private System.Windows.Forms.TextBox txtRutaSelec;
+        private System.Windows.Forms.Button btnElegirCarpetaArchivos;
+    }
+}
+
---
+++ Generar_Factura/Generar_Factura/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Runtime.InteropServices;
+using System.IO;
+using Microsoft.VisualBasic.FileIO;
+using System.Security.AccessControl;
+using System.Security.Principal;
+
+namespace Generar_Factura
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEjecutar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string strRutaArchivos = this.txtRutaArchivos.Text.ToString().Trim();
+
+                if (strRutaArchivos == string.Empty)
+                {
+                    MessageBox.Show("Se requiere de la ruta de los archivos a examinar", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    txtRutaArchivos.Focus();
+                    return;
+                }
+
+                string strArchivos = string.Empty;
+                //SE OBTIENEN TODOS LOS ARCHIVOS DE LA RUTA
+                string[] arrArchivosPendi = Directory.GetFiles(strRutaArchivos);
+                foreach (string strArchivo in arrArchivosPendi)
+                {
+                    // SE OBTIENE LA INFORMACION DEL ARCHIVO
+                    FileInfo infoArchivo = new FileInfo(strArchivo);
+
+                    strArchivos += infoArchivo.Name + "|";
+
+                }
+
+                string[] arrArchivos = strArchivos.Split('|');
+
+
+                string strRutaArchivoGuardar = this.txtRutaSelec.Text.ToString().Trim();
+                if (strRutaArchivoGuardar.Trim() == string.Empty)
+                {
+                    MessageBox.Show("Antes de generar el archivo, seleccione una carpeta en donde se guardara la factura.", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    this.btnElegirCarpeta.Focus();
+                    return;
+                }
+
+                string strFileFactura = "Factura_Generada_" + DateTime.Now.Day +DateTime.Now.Month  + DateTime.Now.Year + DateTime.Now.Minute +".csv";
+                string strPathFactura = strRutaArchivoGuardar + "\\" + strFileFactura;
+
+                if (!File.Exists(strPathFactura))
+                {
+                    try { File.Create(strPathFactura).Dispose(); }
+                    catch (Exception exc)
+                    { }
+                }
+                else
+                {
+                    MessageBox.Show("La Factura '" + strFileFactura + "' ya existe en la ruta '" + strRutaArchivoGuardar + "'. Seleccione otra carpeta para guardar el archivo.", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    this.btnElegirCarpeta.Focus();
+                    return;
+                }
+
+
+                try
+                {
+                    foreach (string strArchivo in arrArchivos)
+                    {
+                        if (strArchivo == string.Empty)
+                            continue;
+
+                        string strArchivosAExaminar = strRutaArchivos + "\\" + strArchivo;
+                        using (TextFieldParser tfpParsecsv = new TextFieldParser(strArchivosAExaminar, Encoding.Default,false))
+                        {
+                            tfpParsecsv.CommentTokens = new string[] { "" };
+                            tfpParsecsv.SetDelimiters(new string[] { "," });
+                            tfpParsecsv.HasFieldsEnclosedInQuotes = true;
+
+                            
+
+                            int iFila = 0;
+
+                            while (!tfpParsecsv.EndOfData)
+                            {
+                                string strRenglon = string.Empty;
+                                string[] arrColumnas = tfpParsecsv.ReadFields();
+                                iFila++;
+
+                                if (iFila == 1)
+                                    continue;
+
+                                foreach (string strFields in arrColumnas)
+                                {
+                                    if (strFields.Contains(","))
+                                        strRenglon += "\"" + strFields + "\",";
+                                    else
+                                        strRenglon += strFields + ",";
+                                }
+                                strRenglon = strRenglon.Substring(0, strRenglon.Length - 1);
+
+                                System.IO.StreamWriter stArchivosProc = new System.IO.StreamWriter(strPathFactura, true, Encoding.Default);
+                                stArchivosProc.WriteLine(strRenglon);
+                                stArchivosProc.Close();
+
+                                if (iFila > 10)
+                                    break;
+                            }
+                        }
+                    }
+
+                    MessageBox.Show("Se genero la factura correctamente.", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show("No se puede generar el archivo por el siguiente motivo: \r\n\r\n" + exc.Message, "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+
+            }
+            catch (Exception exc)
+            {
+
+            }
+        }
+
+        private void btnElegirCarpeta_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FolderBrowserDialog fbdMostrarBroDialog = new FolderBrowserDialog();
+
+                if (fbdMostrarBroDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string strRutaElegida = fbdMostrarBroDialog.SelectedPath;
+                    this.txtRutaSelec.Text = strRutaElegida;
+                }
+            }
+            catch (Exception exc)
+            {
+
+            }
+        }
+
+        private void btnElegirCarpetaArchivos_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FolderBrowserDialog fbdMostrarBroDialog = new FolderBrowserDialog();
+
+                if (fbdMostrarBroDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string strRutaElegida = fbdMostrarBroDialog.SelectedPath;
+                    this.txtRutaArchivos.Text = strRutaElegida;
+                }
+            }
+            catch (Exception exc)
+            {
+                
+            }
+        }
+
+
+    }
+}
